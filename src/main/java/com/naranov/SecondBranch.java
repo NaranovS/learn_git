f785bf9cb551b57c0f62a2157a7a96854f8a40fd
Sergey Naranov <ssnarano@gmail.com>
2019-10-16 23:02:51 +0300

    commit from master

--- src/main/java/com/naranov/SecondBranch.java
+++ src/main/java/com/naranov/SecondBranch.java
@@ -2,4 +2,10 @@ package com.naranov;
 
 public class SecondBranch {
     private int id;
+
+    // code from third branch
+    private String third;
+
+    // code from master
+    private int digital;
 }
